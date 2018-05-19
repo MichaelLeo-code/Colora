67a3be1e0ca930d05825ded394ab9a887d816a7c
Michael <michael.leo.ee@gmail.com>
2018-05-19 11:04:03 +0300

    Start working on switching features

--- app/src/main/java/com/example/michael/test2/Library.java
+++ app/src/main/java/com/example/michael/test2/Library.java
@@ -1,5 +1,6 @@
 package com.example.michael.test2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
@@ -9,34 +10,82 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Library extends AppCompatActivity {
     private ImageView test;
-    private Button GoBackButton;
+    private Button goBackButton;
+    private String filename = "myfile";
+    private String fileContents = "Hello world!";
+    private Button saveSomeDataButton;
+    private TextView testText;
+    private String getText;
+    private Button showText;
+    private BufferedReader in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
-        GoBackButton = findViewById(R.id.go_back_button);
+        goBackButton = findViewById(R.id.go_back_button);
+        saveSomeDataButton = findViewById(R.id.save_button);
+        testText = findViewById(R.id.saved_data_text);
+        showText = findViewById(R.id.show_text);
 
-        GoBackButton.setOnClickListener(new View.OnClickListener() {
+        goBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Library.this, MainActivity.class));
             }
         });
+
+        saveSomeDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveDataTest();
+            }
+        });
+
+        showText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BufferedReader in = null;
+                try {
+                    in = new BufferedReader(new InputStreamReader(openFileInput(filename)));
+                    getText = in.readLine();
+                    testText.setText(getText);
+                } catch (IOException e) {
+                    testText.setText("Error");
+                } finally {
+                    if (in != null) {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            // ignore
+                        }
+                    }
+                }
+            }
+        });
     }
-    public void animate(View view) {
-        test = findViewById(R.id.imageView);
-        Drawable d = test.getDrawable();
-        if (d instanceof AnimatedVectorDrawableCompat){
-            AnimatedVectorDrawableCompat avd = (AnimatedVectorDrawableCompat) d;
-            avd.start();
-        }else if (d instanceof AnimatedVectorDrawable){
-            AnimatedVectorDrawable avd = (AnimatedVectorDrawable) d;
-            avd.start();
+
+    private void saveDataTest(){
+        try {
+            FileOutputStream outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(fileContents.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            testText.setText("error");
         }
     }
 }
